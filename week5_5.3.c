200dcf29d78a71c3c79bfe206af64f72e188af5c
David Kevork <davidkevork@users.noreply.github.com>
2018-03-23 19:28:34 +1100

    Update week5_5.3.c

--- week5_5.3.c
+++ week5_5.3.c
@@ -9,14 +9,14 @@ int n(char* a, int b) {
 }
 
 // https://stackoverflow.com/a/4003287
-int mod (int a, int b)
+int mod(int a, int b)
 {
-   if(b < 0)
-     return mod(a, -b);   
-   int ret = a % b;
-   if(ret < 0)
-     ret+=b;
-   return ret;
+	if(b < 0)
+		return mod(a, -b);   
+	int ret = a % b;
+	if(ret < 0)
+		ret+=b;
+	return ret;
 }
 
 int r(int x) {
@@ -28,22 +28,22 @@ int s(int x) {
 }
 
 
-void encrypt(char* x, char* m) {
+void e(char* x, char* m) {
 	m[0] = r(n(x,0)) + '0';
 	m[1] = r(n(x,1)) + '0';
 	m[2] = r(n(x,2)) + '0';
 	m[3] = r(n(x,3)) + '0';
 
-	int two = n(m,1);
+	int a = n(m,1);
 	m[1] = n(m, 3)+'0';
-	m[3] = two+'0';
+	m[3] = a+'0';
 
-	int zero = n(m,0);
+	int b = n(m,0);
 	m[0] = n(m, 2)+'0';
-	m[2] = zero+'0';
+	m[2] = b+'0';
 }
 
-void decrypt(char* x, char* m) {
+void d(char* x, char* m) {
 	m[0] = s(n(x,2))+'0';
 	m[1] = s(n(x,3))+'0';
 	m[2] = s(n(x,0))+'0';
@@ -51,18 +51,18 @@ void decrypt(char* x, char* m) {
 }
 
 int main() {
-	char digit[5];
+	char dn[5];
 	printf("Please input the number:");
-	scanf("%4[0-9]", &digit);
-	printf("digit=%s\n", digit);
+	scanf("%4[0-9]", &dn);
+	printf("digit=%s\n", dn);
 	
-	char encrypted[5];
-	encrypt(digit, encrypted);
-	printf("encrypt=%c%c%c%c\n", encrypted[0], encrypted[1], encrypted[2], encrypted[3]);
+	char e[5];
+	e(dn, e);
+	printf("encrypt=%c%c%c%c\n", e[0], e[1], e[2], e[3]);
 
-	char decrypted[5];
-	decrypt(encrypted, decrypted);
-	printf("decrypt=%c%c%c%c\n", decrypted[0], decrypted[1], decrypted[2], decrypted[3]);
+	char d[5];
+	d(e, d);
+	printf("decrypt=%c%c%c%c\n", d[0], d[1], d[2], d[3]);
 	
 	return 0;
 }
