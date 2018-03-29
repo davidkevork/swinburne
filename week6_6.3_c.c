69c14d22fe54ef187656486015301afc7db12cec
David Kevork <davidkevork@users.noreply.github.com>
2018-03-29 15:24:40 +1100

    Update week6_6.3_c.c

--- week6_6.3_c.c
+++ week6_6.3_c.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-bool check_total(int array[], int array_size) {
-	int sum = 0;
-	for (int i = 0; i < array_size; i++)
-	{
-		sum += array[i];
-	}
-	if (sum >= 100) {
+bool check_total(int total) {
+	if (total >= 100) {
 		return true;
 	} else {
 		return false;
@@ -17,6 +12,6 @@ bool check_total(int array[], int array_size) {
 int main(int argc, char const *argv[])
 {
 	int array[10] = {5, 8, 11, 13, 15, 16, 23, 38, 45, 48};
-	printf("%i\n", check_total(array, 10));
+	printf("%i\n", check_total(222, 10));
 	return 0;
 }
