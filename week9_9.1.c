8a943e3cba8e235fb9b602ba6ccbd1046bc2bec4
David Kevork <davidkevork@users.noreply.github.com>
2018-04-27 15:02:44 +1000

    Update week9_9.1.c

--- week9_9.1.c
+++ week9_9.1.c
@@ -5,7 +5,6 @@ void swap(int *p1, int *p2);
 int main(){
  int x = 10;
  int y = 20;
- // You must figure out how to call the function correctly!
  swap(&x, &y);
  // Should print out x: 20, y: 10
  printf("x: %d, y: %d\n", x, y);
