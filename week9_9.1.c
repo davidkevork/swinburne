c488c46a3f531d10123685d33b31004c65e6dff5
David Kevork <davidkevork@users.noreply.github.com>
2018-04-27 14:59:58 +1000

    Create week9_9.1.c

---
+++ week9_9.1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void swap(int *p1, int *p2);
+
+int main(){
+ int x = 10;
+ int y = 20;
+ // You must figure out how to call the function correctly!
+ swap(&x, &y);
+ // Should print out x: 20, y: 10
+ printf("x: %d, y: %d\n", x, y);
+}
+
+void swap(int *p1, int *p2) {
+  if (*p1 < *p2) {
+    int temp = *p1;
+    *p1 = *p2;
+    *p2 = temp;
+  }
+}
