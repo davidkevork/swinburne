243a0f75202450156a3d0b4de7a828f61ab36604
David Kevork <davidkevork@users.noreply.github.com>
2018-05-11 13:58:31 +1000

    Update assignment2_qn5.c

--- assignment2_qn5.c
+++ assignment2_qn5.c
@@ -8,10 +8,10 @@
 int get_menu();
 int get_unit();
 int get_calculation();
-float diameter_to_radius(float diameter);
-float random_value_generator(int min, int max);
-float area(float radius);
-float circumference(float radius);
+double diameter_to_radius(double diameter);
+double random_value_generator(int min, int max);
+double area(double radius);
+double circumference(double radius);
 
 int main()
 {
@@ -21,33 +21,32 @@ int main()
     do {
         menu = get_menu();
         switch (menu) {
-            case -1:
-                return 0;
             case 1:
                 unit = get_unit();
-                float radius = random_value_generator(5, 15);
+                double radius = random_value_generator(5, 15);
                 calc = get_calculation();
                 printf("Radius = %f\n", radius);
                 calc == 0 ?
-                printf("Area = %f\n", area(radius))
+                printf("Area = %f%s\n", area(radius), (unit == 0 ? "cm" : "m"))
                           :
-                printf("Circumference = %f\n", circumference(radius));
+                printf("Circumference = %f%s\n", circumference(radius), (unit == 0 ? "cm" : "m"));
                 break;
             case 2:
                 unit = get_unit();
-                float diameter = random_value_generator(15, 30);
+                double diameter = random_value_generator(15, 30);
                 calc = get_calculation();
                 printf("Diameter = %f\n", diameter);
                 calc == 0 ?
-                printf("Area = %f\n", area(diameter_to_radius(diameter)))
+                printf("Area = %f%s\n", area(diameter_to_radius(diameter)), (unit == 0 ? "cm" : "m"))
                           :
-                printf("Circumference = %f\n", circumference(diameter_to_radius(diameter)));
+                printf("Circumference = %f%s\n", circumference(diameter_to_radius(diameter)), (unit == 0 ? "cm" : "m"));
                 break;
             default:
                 printf("Enter a valid number.\n");
                 break;
         }
     } while (menu != -1);
+    return 0;
 }
 
 /**
@@ -70,29 +69,33 @@ int get_menu() {
  * @return unit
  */
 int get_unit() {
-    unsigned u;
-    printf("Choose a unit.\n");
-    printf("0 for cm\n");
-    printf("1 for m\n");
-    scanf("%u", &u);
-    return (u != 0 && u != 1 ? get_unit() : u);
+    char unit[256];
+    printf("Choose a unit, cm or m:");
+    scanf("%s", unit);
+    if (strcmp(unit, "cm") == 0) {
+        return 0;
+    } else if (strcmp(unit, "m") == 0) {
+        return 1;
+    } else {
+        return get_unit();
+    }
 }
 
 /**
  * Gets calculation from user input
- * User enters [A] for area and [C] for circumference
+ * User enters [A/a] for area and [C/c] for circumference
  * 0 for area / 1 for circumference
  * @return calculation
  */
 int get_calculation() {
-    char calc[1];
+    char calc[256];
     printf("Enter calculation type.\n");
-    printf("[A] for area\n");
-    printf("[C] for circumference\n");
+    printf("[A/a] for area\n");
+    printf("[C/c] for circumference\n");
     scanf("%s", calc);
-    if (strcmp(calc, "A") || strcmp(calc, "a")) {
+    if (strcmp(calc, "A") == 0 || strcmp(calc, "a") == 0) {
         return 0;
-    } else if (strcmp(calc, "C") || strcmp(calc, "c")) {
+    } else if (strcmp(calc, "C") == 0 || strcmp(calc, "c") == 0) {
         return 1;
     } else {
         return get_calculation();
@@ -104,7 +107,7 @@ int get_calculation() {
  * @param diameter
  * @return radius
  */
-float diameter_to_radius(float diameter)
+double diameter_to_radius(double diameter)
 {
     return diameter/2;
 }
@@ -115,7 +118,7 @@ float diameter_to_radius(float diameter)
  * @param max
  * @return random
  */
-float random_value_generator(int min, int max)
+double random_value_generator(int min, int max)
 {
     srand(time(NULL));
     return rand()%(max-min+1)+min;
@@ -127,7 +130,7 @@ float random_value_generator(int min, int max)
  * @param radius
  * @return area
  */
-float area(float radius)
+double area(double radius)
 {
     return M_PI * radius * radius;
 }
@@ -137,7 +140,7 @@ float area(float radius)
  * @param radius
  * @return circumference
  */
-float circumference(float radius)
+double circumference(double radius)
 {
     return 2 * M_PI * radius;
 }
