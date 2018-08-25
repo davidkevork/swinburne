d71dad1143f836a49f7f35a83394ca4cbfd0f7ae
David Kevork <davidkevork@hotmail.com>
2018-08-25 21:55:17 +1000

    Add COS10007 assignment 2 qn 3

---
+++ COS10007_assignment_2_3.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    unsigned int width = 0;
+    unsigned int row = 0;
+    std::cout << "Width of the shape? (Enter odd number only) ";
+    std::cin >> width;
+    std::cout << "Enter row number to get sum: ";
+    std::cin >> row;
+
+    int space = (width - 1) / 2;
+    
+    for(size_t i = 1; i <= row; i++)
+    {
+        for(size_t j = 1; j <= (2 * space); j++)
+        {
+            std::cout << " ";
+        }
+        space--;
+        for(size_t k = 1; k < 2 * i; k++)
+        {
+            std::cout << " " << k;
+        }
+        std::cout << std::endl;
+    }
+
+    space = 1;
+    
+    for(size_t l = 1; l < row ; l++)
+    {
+        for(size_t m = 0; m < space; m++)
+        {
+            std::cout << "  ";
+        }
+        space++;
+        for(size_t n = 1; n <= 2 * (row - l ) - 1; n++)
+        {
+            std::cout << " " << n;
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << "The sum of row " << row << " is " << row * width << std::endl;
+
+    return 0;
+}
