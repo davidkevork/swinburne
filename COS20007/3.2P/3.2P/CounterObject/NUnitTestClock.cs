925fdf583b216111519e5bba3a2cf4d0a61a6e3b
David Kevork <davidkevork@hotmail.com>
2019-03-24 21:50:20 +1100

    Upload  COS20007 3.2P

---
+++ COS20007/3.2P/3.2P/CounterObject/NUnitTestClock.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using System;
+namespace CounterObject
+{
+    [TestFixture()]
+    public class NUnitTestClock
+    {
+        [Test()]
+        public void TestClock()
+        {
+            Clock c = new Clock();
+
+            Assert.AreEqual("00:00:00", c.Time, "Should initialize and get time at start");
+        }
+
+        [Test()]
+        public void Test_tick_Elapsed()
+        {
+            Clock c = new Clock();
+            double d = 0;
+            for (int i = 0; i < 200 * 1000 * 1000; i++)
+            {
+                d += 1;
+            }
+            Assert.AreEqual("00:00:05", c.Time, "Should get time at 5 second");
+        }
+
+        [Test()]
+        public void TestReset()
+        {
+            Clock c = new Clock();
+            double d = 0;
+            for (int i = 0; i < 200 * 1000 * 1000; i++)
+            {
+                d += 1;
+            }
+            c.Reset();
+
+            Assert.AreEqual("00:00:00", c.Time, "Should reset time");
+        }
+    }
+}
---
+++ COS20007/3.2P/3.2P/CounterObject/NUnitTestCounter.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+namespace CounterObject
+{
+    [TestFixture()]
+    public class NUnitTestCounter
+    {
+        [Test()]
+        public void TestCounter()
+        {
+            Counter VCounter = new Counter("Counter");
+
+            Assert.AreEqual(0, VCounter.Value, "Initialising the counter starts at 0");
+        }
+
+        [Test()]
+        public void TestIncrement()
+        {
+            Counter VCounter = new Counter("Counter");
+            VCounter.Increment();
+
+            Assert.AreEqual(1, VCounter.Value, "Incrementing the counter adds one to the count");
+        }
+
+        [Test()]
+        public void TestIncrementMultiple()
+        {
+            Counter VCounter = new Counter("Counter");
+            VCounter.Increment();
+            VCounter.Increment();
+            VCounter.Increment();
+
+            Assert.AreEqual(3, VCounter.Value, "Increments get multiple times increases the counter to match");
+        }
+
+        [Test()]
+        public void TestReset()
+        {
+            Counter VCounter = new Counter("Counter");
+            VCounter.Increment();
+            VCounter.Increment();
+            VCounter.Reset();
+
+            Assert.AreEqual(0, VCounter.Value, "Resetting the timer sets the count to 0");
+        }
+    }
+}
