3dcedd41c7eb25f1e91e293a9c36dbc87597028c
David Kevork <davidkevork@hotmail.com>
2019-04-06 15:51:10 +1100

    Upload 5.2C

---
+++ COS20007/5.2C/GameMain.cs
@@ -0,0 +1,100 @@
+using System;
+using SwinGameSDK;
+
+namespace MyGame
+{
+    public class GameMain
+    {
+        private enum ShapeKind
+        {
+            Rectangle,
+            Circle,
+            Line
+        }
+
+        public static void Main ()
+        {
+            Drawing myDrawing = new Drawing ();
+            ShapeKind KindToAdd = ShapeKind.Circle;
+
+            //Open the game window
+            SwinGame.OpenGraphicsWindow ("GameMain", 800, 600);
+            SwinGame.ShowSwinGameSplashScreen ();
+            
+            //Run the game loop
+            while(SwinGame.WindowCloseRequested () == false)
+            {
+                //Fetch the next batch of UI interaction
+                SwinGame.ProcessEvents ();
+                
+                //Clear the screen and myDrawing the framerate
+                SwinGame.ClearScreen (Color.White);
+
+                if (SwinGame.KeyTyped (KeyCode.vk_r))
+                {
+                    KindToAdd = ShapeKind.Rectangle;
+                }
+                else if (SwinGame.KeyTyped (KeyCode.vk_c))
+                {
+                    KindToAdd = ShapeKind.Circle;
+                }
+                else if (SwinGame.KeyTyped (KeyCode.vk_l))
+                {
+                    KindToAdd = ShapeKind.Line;
+                }
+
+                if (SwinGame.MouseClicked (MouseButton.LeftButton))
+                {
+                    Shape newShape;
+
+                    switch (KindToAdd)
+                    {
+                        case ShapeKind.Circle:
+                            newShape = new Circle ();
+                            break;
+                        case ShapeKind.Rectangle:
+                            newShape = new Rectangle ();
+                            break;
+                        case ShapeKind.Line:
+                            newShape = new Line ();
+                            break;
+                        default:
+                            newShape = new Rectangle ();
+                            break;
+                    }
+
+                    newShape.X = SwinGame.MouseX ();
+                    newShape.Y = SwinGame.MouseY ();
+                    myDrawing.AddShape (newShape);
+                }
+                if (SwinGame.MouseClicked (MouseButton.RightButton)) {
+                    myDrawing.SelectShapesAt (SwinGame.MousePosition ());
+                }
+                if (SwinGame.KeyTyped (KeyCode.vk_DELETE) || SwinGame.KeyTyped (KeyCode.vk_BACKSPACE)) {
+                    foreach (Shape shape in myDrawing.SelectedShapes) {
+                        myDrawing.RemoveShape (shape);
+                    }
+                }
+                if (SwinGame.KeyTyped (KeyCode.vk_SPACE)) {
+                    myDrawing.Background = SwinGame.RandomRGBColor (255);
+                }
+                if (SwinGame.KeyTyped (KeyCode.vk_s)) {
+                    myDrawing.Save ("./drawing.txt");
+                }
+                if (SwinGame.KeyTyped (KeyCode.vk_o)) {
+                    try {
+                        myDrawing.Load ("./drawing.txt");
+                    } catch (Exception e) {
+                        Console.Error.WriteLine ("Error loading file: {0}", e.Message);
+                    }
+
+                }
+                myDrawing.Draw ();
+                SwinGame.DrawFramerate (0,0);
+
+                //myDrawing onto the screen
+                SwinGame.RefreshScreen (60);
+            }
+    }
+}
+}
---
+++ COS20007/5.2C/Line.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using SwinGameSDK;
+using System.IO;
+
+namespace MyGame
+{
+    public class Line : Shape
+    {
+        private float _endX, _endY;
+
+        public Line (Color c, float startX, float startY, float endX, float endY)
+        {
+            //
+        }
+
+        public Line ()
+        {
+            //
+        }
+
+        public float EndX
+        {
+            get
+            {
+                return _endX;
+            }
+            set
+            {
+                _endX = value;
+            }
+        }
+
+        public float EndY
+        {
+            get
+            {
+                return _endY;
+            }
+            set
+            {
+                _endY = value;
+            }
+        }
+
+        public override void Draw ()
+        {
+            SwinGame.DrawLine (Color, X, Y, _endX, _endY);
+            if (base.Selected) {
+                DrawOutline ();
+            }
+        }
+
+        public override void DrawOutline () 
+        {
+            SwinGame.DrawCircle (Color.Black, X, Y, 2);
+        }
+
+        public override Boolean IsAt (Point2D pt)
+        {
+            return SwinGame.PointOnLine (pt, X, Y, _endX, _endY);
+        }
+
+        public override void SaveTo (StreamWriter writer)
+        {
+            writer.WriteLine ("Line");
+            base.SaveTo (writer);
+            writer.WriteLine (EndX);
+            writer.WriteLine (EndY);
+        }
+
+        public override void LoadFrom (StreamReader reader)
+        {
+            base.LoadFrom (reader);
+            EndX = reader.ReadInteger ();
+            EndY = reader.ReadInteger ();
+        }
+    }
+}
